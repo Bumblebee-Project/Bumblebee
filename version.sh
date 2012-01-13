5c5474e22e0ca295ad80fdc7bf12b77b21751a8a
Lekensteyn <lekensteyn@gmail.com>
2012-01-13 23:06:56 +0100

    Update version to 2.99, add version script

---
+++ version.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+rootdir="$1"
+[ -n "$rootdir" ] || rootdir=.
+if [ -z "$PACKAGE_VERSION" ]; then
+	eval "$(cd "$rootdir" && grep -m1 PACKAGE_VERSION configure)"
+fi
+
+if [ -d "$rootdir/.git" ]; then
+	VN=$(cd "$rootdir" &&
+		git describe --tags --match 'v[0-9]*' 2>/dev/null)
+
+	if [ -z "$VN" ]; then
+		VN=$(cd "$rootdir" &&
+			git log -1 --pretty=format:"git-%cd-%h" --date=short \
+			2>/dev/null)
+		[ -z "$VN" ] || VN="$PACKAGE_VERSION-$VN"
+	fi
+else
+	srcdir="$(cd "$1" && pwd)"
+	case "$srcdir" in
+	  */*-*-[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f])
+		git_hash="$(srcdir##*-)"
+		[ -z "$PACKAGE_VERSION" ] || VN="$PACKAGE_VERSION-$git_hash"
+		;;
+	esac
+fi
+
+VN=$(expr "$VN" : v*'\(.*\)')
+[ -n "$VN" ] || VN="$PACKAGE_VERSION"
+
+if [ -n "$VN" ]; then
+	echo "$VN"
+else
+	echo "0-UNKNOWN"
+fi
