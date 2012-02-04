fdc5856e43e89467a62c6f2245fb0658aac7e542
Lekensteyn <lekensteyn@gmail.com>
2012-02-05 00:48:55 +0100

    Use GLib for process spawning, drop SIGCHLD handler

--- src/bbrun.c
+++ src/bbrun.c
@@ -24,14 +24,12 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <string.h>
-#include <signal.h>
 #include <sys/wait.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <glib.h>
 #include "bbrun.h"
 #include "bblogger.h"
 
@@ -94,40 +92,10 @@ static int pidlist_find(pid_t findpid) {
   return 0;
 }//pidlist_find
 
-static void childsig_handler(int signum) {
-  if (signum != SIGCHLD) {
-    return;
-  }
-  int chld_stat = 0;
-  /* Wait for the child to exit */
-  pid_t ret = wait(&chld_stat);
-  /* Log the child termination and return value */
-  if (ret == -1) {
-    bb_log(LOG_DEBUG, "SIGCHILD received, but wait failed with %s\n",
-            strerror(errno));
-  } else if (WIFEXITED(chld_stat)) {
-    bb_log(LOG_DEBUG, "Process with PID %i returned code %i\n", ret,
-            WEXITSTATUS (chld_stat));
-  } else if (WIFSIGNALED(chld_stat)) {
-    bb_log(LOG_DEBUG, "Process with PID %i terminated with %i\n", ret,
-            WTERMSIG(chld_stat));
-  }
-  pidlist_remove(ret);
-}//childsig_handler
-
-static void check_handler(void) {
-  // Set handler for this child process if not already
-  if (handler_set == 0) {
-    struct sigaction new_action;
-    new_action.sa_handler = childsig_handler;
-    sigemptyset(&new_action.sa_mask);
-    new_action.sa_flags = 0;
-    sigaction(SIGCHLD, &new_action, NULL);
-    handler_set = 1;
-  }
-}//check_handler
-
-static void bb_run_exec_detached(char **argv);
+static void child_died_handler(GPid pid, gint status, gpointer data) {
+  bb_log(LOG_DEBUG, "Process %i (%s) died\n", pid, data);
+  pidlist_remove(pid);
+}
 
 /**
  * Forks and runs the given application and waits for the process to finish
@@ -138,43 +106,62 @@ static void bb_run_exec_detached(char **argv);
  */
 int bb_run_fork(char **argv, int detached) {
   int exitcode = -1;
+  int status;
+  GSpawnFlags flags;
+  GPid pid;
+  GError *error = NULL;
 
-  check_handler();
-  /* Fork and attempt to run given application */
-  pid_t pid = fork();
-  if (pid == 0) {
-    /* child process after fork */
-    if (detached) {
-      bb_run_exec_detached(argv);
-    } else {
-      bb_run_exec(argv);
+  flags = G_SPAWN_SEARCH_PATH;
+  flags |= G_SPAWN_DO_NOT_REAP_CHILD;
+  if (detached) {
+    flags |= G_SPAWN_STDERR_TO_DEV_NULL;
+    flags |= G_SPAWN_STDOUT_TO_DEV_NULL;
+  } else {
+    flags |= G_SPAWN_CHILD_INHERITS_STDIN;
+  }
+  if (!g_spawn_async(NULL, argv, NULL, flags, NULL, NULL, &pid, &error)) {
+    bb_log(LOG_DEBUG, "Process %s failed with %s\n", argv[0], error->message);
+    return -1;
+  }
+  bb_log(LOG_DEBUG, "Process %s started, PID %i.\n", argv[0], pid);
+  pidlist_add(pid);
+
+  if (waitpid(pid, &status, 0) != -1) {
+    if (WIFEXITED(status)) {
+      /* program exited normally, return status */
+      exitcode = WEXITSTATUS(status);
+    } else if (WIFSIGNALED(status)) {
+      /* program was terminated by a signal */
+      exitcode = 128 + WTERMSIG(status);
     }
-  } else if (pid > 0) {
-    /* parent process after fork */
-    int status = 0;
+  } else {
+    bb_log(LOG_ERR, "waitpid(%i) failed with %s\n", pid, strerror(errno));
+  }
+  pidlist_remove(pid);
+  return exitcode;
+}
 
-    bb_log(LOG_DEBUG, "Process %s started, PID %i.\n", argv[0], pid);
-    pidlist_add(pid);
-
-    if (waitpid(pid, &status, 0) != -1) {
-      if (WIFEXITED(status)) {
-        /* program exited normally, return status */
-        exitcode = WEXITSTATUS(status);
-      } else if (WIFSIGNALED(status)) {
-        /* program was terminated by a signal */
-        exitcode = 128 + WTERMSIG(status);
+static void set_ld_library_path(gpointer path) {
+  char *ldpath = path;
+  if (ldpath && *ldpath) {
+    char *current_path = getenv("LD_LIBRARY_PATH");
+    /* Fork went ok, set environment if necessary */
+    if (current_path) {
+      char *ldpath_new = malloc(strlen(ldpath) + 1 + strlen(current_path) + 1);
+      if (ldpath_new) {
+        strcpy(ldpath_new, ldpath);
+        strcat(ldpath_new, ":");
+        strcat(ldpath_new, current_path);
+        setenv("LD_LIBRARY_PATH", ldpath_new, 1);
+        free(ldpath_new);
+      } else {
+        bb_log(LOG_WARNING, "Could not allocate memory for LD_LIBRARY_PATH\n");
       }
     } else {
-      bb_log(LOG_ERR, "waitpid(%i) faild with %s\n", pid, strerror(errno));
+      setenv("LD_LIBRARY_PATH", ldpath, 1);
     }
-    pidlist_remove(pid);
-  } else {
-    /* Fork failed */
-    bb_log(LOG_ERR, "Process %s could not be started. fork() failed.\n", argv[0]);
   }
 
-  /* could not determine return value */
-  return exitcode;
 }
 
 /**
@@ -189,49 +176,27 @@ int bb_run_fork(char **argv, int detached) {
  * @return The childs process ID
  */
 pid_t bb_run_fork_ld_redirect(char **argv, char *ldpath, int redirect) {
-  check_handler();
-  // Fork and attempt to run given application
-  pid_t ret = fork();
-  if (ret == 0) {
-    if (ldpath && *ldpath) {
-      char *current_path = getenv("LD_LIBRARY_PATH");
-      /* Fork went ok, set environment if necessary */
-      if (current_path) {
-        char *ldpath_new = malloc(strlen(ldpath) + 1 + strlen(current_path) + 1);
-        if (ldpath_new) {
-          strcpy(ldpath_new, ldpath);
-          strcat(ldpath_new, ":");
-          strcat(ldpath_new, current_path);
-          setenv("LD_LIBRARY_PATH", ldpath_new, 1);
-          free(ldpath_new);
-        } else {
-          bb_log(LOG_WARNING, "Could not allocate memory for LD_LIBRARY_PATH\n");
-        }
-      } else {
-        setenv("LD_LIBRARY_PATH", ldpath, 1);
-      }
-    }
-    //open /dev/null for stdin redirect
-    int devnull = open("/dev/null", O_RDWR);
-    //fail silently on error, nothing we can do about it anyway...
-    if (devnull >= 0){dup2(devnull, STDIN_FILENO);}
-    //redirect stdout and stderr to the given filenum.
-    dup2(redirect, STDOUT_FILENO);
-    dup2(redirect, STDERR_FILENO);
-    //ok, all ready, now actually execute
-    bb_run_exec(argv);
-  } else {
-    if (ret > 0) {
-      // Fork went ok, parent process continues
-      bb_log(LOG_DEBUG, "Process %s started, PID %i.\n", argv[0], ret);
-      pidlist_add(ret);
-    } else {
-      // Fork failed
-      bb_log(LOG_ERR, "Process %s could not be started. fork() failed.\n", argv[0]);
-      return 0;
-    }
+  gint standard_output = -1, standard_error = -1;
+  GSpawnFlags flags;
+  GPid pid;
+  GError *error = NULL;
+
+  flags = G_SPAWN_SEARCH_PATH;
+  flags |= G_SPAWN_DO_NOT_REAP_CHILD;
+  if (!g_spawn_async_with_pipes(NULL, argv, NULL, flags,
+          (GSpawnChildSetupFunc)set_ld_library_path, ldpath, &pid,
+          NULL, &standard_output, &standard_error,
+          &error)) {
+    bb_log(LOG_DEBUG, "Process %s failed with %s\n", argv[0], error->message);
+    return 0;
   }
-  return ret;
+  bb_log(LOG_DEBUG, "Process %s started, PID %i.\n", argv[0], pid);
+  pidlist_add(pid);
+  /* redirect stdout and stderr to the given fd */
+  dup2(redirect, standard_output);
+  dup2(redirect, standard_error);
+  g_child_watch_add(pid, (GChildWatchFunc)child_died_handler, argv[0]);
+  return pid;
 }
 
 /**
@@ -240,34 +205,28 @@ pid_t bb_run_fork_ld_redirect(char **argv, char *ldpath, int redirect) {
  * @param argv The arguments values, the first one is the application path or name
  */
 void bb_run_fork_wait(char** argv, int timeout) {
-  check_handler();
-  // Fork and attempt to run given application
-  pid_t ret = fork();
-  if (ret == 0) {
-    // Fork went ok, child process replace
-    bb_run_exec(argv);
-  } else {
-    if (ret > 0) {
-      // Fork went ok, parent process continues
-      bb_log(LOG_DEBUG, "Process %s started, PID %i.\n", argv[0], ret);
-      pidlist_add(ret);
-      //sleep until process finishes or timeout reached
-      int i = 0;
-      while (bb_is_running(ret) && ((i < timeout) || (timeout == 0)) && dowait) {
-        usleep(1000000);
-        i++;
-      }
-      //make a single attempt to kill the process if timed out, without waiting
-      if (bb_is_running(ret)) {
-        bb_stop(ret);
-      }
-    } else {
-      // Fork failed
-      bb_log(LOG_ERR, "Process %s could not be started. fork() failed.\n", argv[0]);
-      return;
-    }
+  GSpawnFlags flags;
+  GPid pid;
+  GError *error = NULL;
+
+  flags = G_SPAWN_SEARCH_PATH;
+  flags |= G_SPAWN_DO_NOT_REAP_CHILD;
+  if (!g_spawn_async(NULL, argv, NULL, flags, NULL, NULL, &pid, &error)) {
+    bb_log(LOG_DEBUG, "Process %s failed with %s\n", argv[0], error->message);
+    return;
   }
-  return;
+  bb_log(LOG_DEBUG, "Process %s started, PID %i.\n", argv[0], pid);
+  pidlist_add(pid);
+  g_child_watch_add(pid, (GChildWatchFunc)child_died_handler, argv[0]);
+  int i = 0;
+  while (bb_is_running(pid) && ((i < timeout) || (timeout == 0)) && dowait) {
+    usleep(1000000);
+    i++;
+  }
+  /* make a single attempt to kill the process if timed out, without waiting */
+  if (bb_is_running(pid)) {
+    bb_stop(pid);
+  }
 }
 
 /// Returns 1 if a process is currently running, 0 otherwise.
@@ -324,35 +283,6 @@ void bb_run_exec(char **argv) {
   execvp(argv[0], argv);
   bb_log(LOG_ERR, "Error running \"%s\": %s\n", argv[0], strerror(errno));
   exit(errno);
-}
-
-/**
- * Attempts to run the given application, replacing the current process but
- * redirect all standard in/outputs to /dev/null.
- * @param argv The program to be run
- */
-static void bb_run_exec_detached(char **argv) {
-  int old_stderr, exec_err;
-  bb_log(LOG_DEBUG, "Hiding stderr for execution of %s\n", argv[0]);
-  /* Redirect all three standard file descriptors to /dev/null.
-   * If daemonized, this already happened - but doing it
-   * again doesn't hurt since this fork won't run forever anyway.
-   */
-  int devnull = open("/dev/null", O_RDWR);
-  if (devnull < 0){
-    bb_log(LOG_ERR, "Could not open /dev/null: %s\n", strerror(errno));
-  }
-  old_stderr = dup(STDERR_FILENO);
-  dup2(devnull, STDIN_FILENO);
-  dup2(devnull, STDOUT_FILENO);
-  dup2(devnull, STDERR_FILENO);
-  //done redirecting, do the exec
-  execvp(argv[0], argv);
-  /* note: the below lines are only executed if execvp fails */
-  exec_err = errno;
-  dup2(old_stderr, STDERR_FILENO);
-  bb_log(LOG_ERR, "Error running \"%s\": %s\n", argv[0], strerror(exec_err));
-  exit(exec_err);
 }
 
 /**
