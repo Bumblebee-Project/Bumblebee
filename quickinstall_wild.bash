0c7105a3eae18a99511adf80d0f6f3b33b71be3d
Stefan Helmert <stefan.helmert@informatik.tu-chemnitz.de>
2017-07-22 23:18:23 +0200

    build flag to enable broken distro support (wildcard config) optionally

---
+++ quickinstall_wild.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+autoreconf -fi
+./configure CONF_DRIVER=nvidia CONF_DRIVER_MODULE_NVIDIA=nvidia???? \
+  CONF_PRIMUS_LD_PATH=/usr/lib/x86_64-linux-gnu/primus:/usr/lib/i386-linux-gnu/primus \
+  CONF_LDPATH_NVIDIA=/usr/lib/nvidia????:/usr/lib32/nvidia???? \
+  CONF_MODPATH_NVIDIA=/usr/lib/nvidia????/xorg,/usr/lib/xorg/modules \
+  --enable-wild
+make clean
+make
+sudo make install
+sudo cp scripts/systemd/bumblebeed.service /lib/systemd/system/.
+
+
