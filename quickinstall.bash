22a49b24992466dca35372cf94000f0c7cdd5b41
Stefan Helmert <stefan.helmert@informatik.tu-chemnitz.de>
2017-07-23 01:27:40 +0200

    quickinstall for non wildcard should have usefull non wildcard content

--- quickinstall.bash
+++ quickinstall.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 autoreconf -fi
-./configure CONF_DRIVER=nvidia CONF_DRIVER_MODULE_NVIDIA=nvidia???? \
+./configure CONF_DRIVER=nvidia CONF_DRIVER_MODULE_NVIDIA=nvidia-375 \
   CONF_PRIMUS_LD_PATH=/usr/lib/x86_64-linux-gnu/primus:/usr/lib/i386-linux-gnu/primus \
-  CONF_LDPATH_NVIDIA=/usr/lib/nvidia????:/usr/lib32/nvidia???? \
-  CONF_MODPATH_NVIDIA=/usr/lib/nvidia????/xorg,/usr/lib/xorg/modules \
+  CONF_LDPATH_NVIDIA=/usr/lib/nvidia-375:/usr/lib32/nvidia-375 \
+  CONF_MODPATH_NVIDIA=/usr/lib/nvidia-375/xorg,/usr/lib/xorg/modules \
 
 make clean
 make
